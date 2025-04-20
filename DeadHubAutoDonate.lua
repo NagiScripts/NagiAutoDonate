bdf8bcbf955e049fca8465309af11df13f5956d3
redzhubreal <sigma123alpha123beta123@gmail.com>
2025-04-20 15:54:45 +0530

    Update DeadHubAutoDonate.lua

--- DeadHubAutoDonate.lua
+++ DeadHubAutoDonate.lua
@@ -1,3 +1,4 @@
 --loadstring(game:HttpGet("http://vpaste.net/7FzqB"))()
 --loadstring(game:HttpGet("https://pastefy.app/WQsl4yoc/raw"))()
-loadstring(game:HttpGet("https://pastefy.app/gdtnRDZ0/raw"))()
+--loadstring(game:HttpGet("https://pastefy.app/gdtnRDZ0/raw"))()
+loadstring(game:HttpGet("http://vpaste.net/UOPSM"))()
